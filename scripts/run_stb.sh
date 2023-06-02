6951c285b95d4735883ecf72e6f4a5b7d330714b
Abhirup Patra <abhirup86@users.noreply.github.com>
2023-06-02 00:48:27 -0400

    Update run_stb.sh

--- scripts/run_stb.sh
+++ scripts/run_stb.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 for l in `seq 0 10001` ; do
 
-                #mkdir /terray/data/rdkit_geom/xtb_gn2_conf_calc/conformer_${l}
-                cd /Users/abhiruppatra/Dropbox/terray/data/rdkit_geom/xtb_gn2_conf_calc/conformer_${l}
-                #scp -r /Users/abhiruppatra/Dropbox/terray/data/rdkit_geom/conformers/rdkitconf_${l}.sdf .
+                #mkdir /data/rdkit_geom/xtb_gn2_conf_calc/conformer_${l}
+                cd /data/rdkit_geom/xtb_gn2_conf_calc/conformer_${l}
+                #scp -r /data/rdkit_geom/conformers/rdkitconf_${l}.sdf .
                 #xtb rdkitconf_${l}.sdf --gfn 2 --opt normal > xtbrun.out
                 #rm *.log *restart* charge*
 		en=`grep 'TOTAL ENERGY' xtbrun.out | awk '{print $4}'`
 		echo ${l} ${en}
 		#printf "conformer:" "energy:" "${l}" "${en}"
-                cd /Users/abhiruppatra/Dropbox/terray/data/rdkit_geom/xtb_gn2_conf_calc
+                cd /data/rdkit_geom/xtb_gn2_conf_calc
 done
