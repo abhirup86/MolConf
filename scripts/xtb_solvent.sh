0bdab8733aedfd008f3ed0c3410ce28166d5f658
Abhirup Patra <abhirup86@users.noreply.github.com>
2023-06-02 00:48:03 -0400

    Update xtb_solvent.sh

--- scripts/xtb_solvent.sh
+++ scripts/xtb_solvent.sh
@@ -4,10 +4,10 @@ export OMP_MAX_ACTIVE_LEVELS=1
 export MKL_NUM_THREADS=8
 for l in `seq 7495 10001` ; do
 
-                mkdir /home/abhirup/Dropbox/terray/data/rdkit_geom/xtb_with_implicit_solvent/conformer_solvent_${l}
-                cd /home/abhirup/Dropbox/terray/data/rdkit_geom/xtb_with_implicit_solvent/conformer_solvent_${l}
-                scp -r /home/abhirup/Dropbox/terray/data/rdkit_geom/conformers/rdkitconf_${l}.sdf .
+                mkdir /data/rdkit_geom/xtb_with_implicit_solvent/conformer_solvent_${l}
+                cd /data/rdkit_geom/xtb_with_implicit_solvent/conformer_solvent_${l}
+                scp -r /data/rdkit_geom/conformers/rdkitconf_${l}.sdf .
                 xtb rdkitconf_${l}.sdf --gfn 2 --opt --alpb water > xtbrun_solvent.out
                 rm *.log *restart* charge*
-                cd /home/abhirup/Dropbox/terray/data/rdkit_geom/xtb_with_implicit_solvent
+                cd /data/rdkit_geom/xtb_with_implicit_solvent
 done
